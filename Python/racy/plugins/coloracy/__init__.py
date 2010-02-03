b097f9ad4b54bae3db11d239543906baa1b0b7f7
nicolasph <none@none>
2010-02-03 17:50:59 +0100

    Added 'auto' option t coloracy

--- Python/racy/plugins/coloracy/__init__.py
+++ Python/racy/plugins/coloracy/__init__.py
@@ -20,18 +20,19 @@ class Plugin(racy.rplugins.Plugin):
     name = "coloracy"
 
     options = {
-            base.ENABLE_COLORACY_KEYWORD : 'yes',
+            base.ENABLE_COLORACY_KEYWORD : 'auto',
             base.COLORACY_KEYWORD        : None,
             }
 
     allowed_values = {
-            base.ENABLE_COLORACY_KEYWORD : ['no', 'yes']
+            base.ENABLE_COLORACY_KEYWORD : ['no', 'yes', 'auto']
             }
 
     commandline_opts = [ base.ENABLE_COLORACY_KEYWORD ]
 
     descriptions_opts = {
-            base.ENABLE_COLORACY_KEYWORD : 'Enable/disable coloracy plugin',
+            base.ENABLE_COLORACY_KEYWORD : 'Enable/disable coloracy plugin. '
+                                           '"yes" forces color.',
             base.COLORACY_KEYWORD        : base.COLORACY_PATTERN_HELP,
             }
 
@@ -56,23 +57,30 @@ class Plugin(racy.rplugins.Plugin):
             patterns += base.default_patterns
         return patterns
 
-    def get_proxy_stream(self, stream, user_pattern=None, type='stdout'):
-        if 1 or stream.isatty():
-            return ColorConsoleProxy(
-                    self.stdout,
-                    patterns = user_pattern,
-                    type=type
-                    )
+    def get_proxy_stream(
+            self, stream,
+            user_pattern=None,
+            type='stdout',
+            force=False,
+            ):
+        if force or stream.isatty():
+            return ColorConsoleProxy(self.stdout, patterns=user_pattern,
+                                    type=type)
         return stream
 
-    def has_additive(self, prj):
-        enable = prj.get(base.ENABLE_COLORACY_KEYWORD) == 'yes'
 
-        if enable:
+    def has_additive(self, prj):
+        user_option = prj.get(base.ENABLE_COLORACY_KEYWORD)
+        enable = user_option == 'auto'
+        force  = user_option == 'yes'
+
+        if enable or force:
             user_ptrn = prj.get(base.COLORACY_KEYWORD)
             user_ptrn = self.get_patterns(user_ptrn)
-            sys.stdout = self.get_proxy_stream(self.stdout, user_ptrn)
-            sys.stderr = self.get_proxy_stream(self.stderr, user_ptrn, 'stderr')
+            sys.stdout = self.get_proxy_stream(
+                    self.stdout, user_ptrn, force=force)
+            sys.stderr = self.get_proxy_stream(
+                    self.stderr, user_ptrn, type='stderr', force=force)
         else:
             sys.stdout = self.stdout
             sys.stderr = self.stderr

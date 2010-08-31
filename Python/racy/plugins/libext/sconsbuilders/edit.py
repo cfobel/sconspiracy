25624403fbf7d39a6e3fb709a870de5e37176adf
nphilipps <none@none>
2010-08-31 17:58:55 +0200

    [libext] Added 'exec' mode to Edit builder
    --HG--
    branch : 0.3

--- Python/racy/plugins/libext/sconsbuilders/edit.py
+++ Python/racy/plugins/libext/sconsbuilders/edit.py
@@ -8,20 +8,35 @@ import SCons.Node
 import StringIO
 import functools
 
-def apply_expression(expr, source, target):
+def apply_expression(expr, source, target, mode='eval'):
     # inspired from http://code.activestate.com/recipes/437932/
-    codeobj = compile(expr, 'EditBuilder', 'eval')
+    codeobj = compile(expr, 'EditBuilder', mode)
     output = StringIO.StringIO()
+
+    def exec_mode(_codeobj, _globals, _locals):
+        exec(_codeobj, _globals, _locals)
+        return _locals['line']
+
+    def eval_mode(_codeobj, _globals, _locals):
+        return eval(_codeobj, _globals, _locals)
 
     with open(source, 'rb') as f:
         input = enumerate(f)
 
         write = output.write
+        locals_vars = {}
+        if mode == 'exec':
+            callback = exec_mode
+        else:
+            callback = eval_mode
+
         for numz, line in input:
-            line  = line[:-1]
-            num   = numz + 1
-            words = [w for w in line.strip().split(' ') if len(w)]
-            result =  eval(codeobj, globals(), locals())
+            locals_vars.update({
+                'line'  : line[:-1],
+                'num'   : numz + 1,
+                'words' : [w for w in line.strip().split(' ') if w],
+                })
+            result = callback(codeobj, globals(), locals_vars)
             if result is None or result is False:
                 continue
             elif isinstance(result, list) or isinstance(result, tuple):
@@ -44,7 +59,8 @@ def EditArgs(target, source, env):
     expr = []
     expr.extend(env.get('EXPR',[]))
     expr = map(functools.partial(env.subst, raw=1), expr)
-    return files, expr
+    mode = env.get('MODE','eval')
+    return files, expr, mode
 
 def Edit(target, source, env):
     # we don't use target and source as usual : we may apply several times this
@@ -52,7 +68,7 @@ def Edit(target, source, env):
     # that's not possible for scons
     
 
-    files, expr = EditArgs(target, source, env)
+    files, expr, mode = EditArgs(target, source, env)
 
     for f in files:
         for e in expr:
@@ -65,7 +81,7 @@ def Edit(target, source, env):
 
 def EditString(target, source, env):
     """ Information string for Edit """
-    files, expr = EditArgs(target, source, env)
+    files, expr, mode = EditArgs(target, source, env)
     files = ' '.join(files)
     expr = ' '.join(expr)
     s = ' '.join(['Editing file(s)', files, '<<', expr, '>>'])

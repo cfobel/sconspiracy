a47caed6a0f92e113bfe5d78e704c4c77fecae9d
nphilipps <none@none>
2010-09-02 14:16:53 +0200

    [libext] markers : Added missing files
    --HG--
    branch : 0.3

---
+++ Python/racy/plugins/libext/sconsbuilders/utils/__init__.py
@@ -0,0 +1,49 @@
+
+import SCons
+import pprint
+
+from hashlib import md5
+
+
+
+def write(content, filename):
+    with open(filename, 'w') as f:
+        f.write(content)
+
+###############################################################################
+
+markers = {}
+
+def marker(command, prj, options):
+    res      = '{dir}/{file}'
+    cmd      = '{cmd}_{prj}_{hash}'
+    info     = '{prj}\n{cmd} {opt}'
+    options  = ' '.join(options)
+    digest   = md5(options).hexdigest()
+    filename = cmd.format(
+                 cmd  = command,
+                 prj  = prj,
+                 hash = digest,
+                 )
+    res = res.format(
+            dir  = '${BUILD_DIR}/',
+            file  = filename,
+            )
+    markers[filename] = info.format(
+            cmd  = command,
+            prj  = prj,
+            opt  = options,
+            )
+    return res
+
+###############################################################################
+
+def write_marker(env, filename):
+    content = '{info}\n\n---ENV---\n{env}\n---------'
+    content = content.format(
+            info = markers.get(filename.name, 'Unknown ??'),
+            env = pprint.pformat(env.Dictionary())
+            )
+    write(content, filename.get_abspath())
+    #env.Execute(SCons.Script.Touch(filename))
+

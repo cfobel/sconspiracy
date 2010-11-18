2f5d03ce0fc73c9f724004e0b67c2fd340c4bafb
jweinzorn <none@none>
2010-11-18 11:32:13 +0100

    change bat windows file
    --HG--
    branch : experimental

--- bin/racy.bat
+++ bin/racy.bat
@@ -1,13 +1,4 @@
 @REM Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008 The SCons Foundation
 @REM src/script/scons.bat 3842 2008/12/20 22:59:52 scons
-@echo off
 
-
-python racy 
-
-
-call :returncode
-
-
-
-
+python %~dp0%~n0
